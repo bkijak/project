8fa9ab24ef61feea13326620c26847e39e5b3233
Bartlomiej laptop <b.kijak@uea.ac.uk>
2017-11-23 22:10:47 +0000

    initial project

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".ueaLogo").mouseenter(function(){
+        $("h2").hide();
+    });
+
+    $(".ueaLogo").mouseleave(function(){
+        $("h2").show();
+    })
+});

b448ccb116f29c86927974eb00b2b4e30b8dc760
Bartlomiej <bkijak@uea.ac.uk>
2018-03-12 01:02:38 +0000

    Rewrote some code so it's easier to read. Added a new question to test.

--- static/scripts/test.js
+++ static/scripts/test.js
@@ -1,64 +1,81 @@
-var numOfQuestions = 7;
-var question1 = QType1D(1);
-var question2 = QType2D(2);
-var question3 = QType3(3);
-var question4 = QType4(4);
-var question5 = QType1B(5);
-var question6 = QType2B(6);
-var question7 = QType5(7);
-testQuestions.innerHTML = question1[0];
-testQuestions.innerHTML += question2[0];
-testQuestions.innerHTML += question3[0];
-testQuestions.innerHTML += question4[0];
-testQuestions.innerHTML += question5[0];
-testQuestions.innerHTML += question6[0];
-testQuestions.innerHTML += question7[0];
+//number of questions in the test
+var numOfQuestions = 10;
+
+//1D = binary-binary=decimal
+//1B = binary-binary=binary
+//2D = binary+binary=decimal
+//2B = binary+binary=binary
+//3 = binary to decimal
+//4 = decimal to binary
+//5 = logic gate name
+
+//Initialising a 2d array
+// var questions = Array.from(Array(9), () => new Array(4));
+var questions = [];
+questions[0] = QType3(1);
+questions[1] = QType3(2);
+questions[2] = QType4(3);
+questions[3] = QType4(4);
+questions[4] = QType2D(5);
+questions[5] = QType2B(6);
+questions[6] = QType1D(7);
+questions[7] = QType1B(8);
+questions[8] = QType5(9);
+questions[9] = QType6(10);
+testQuestions.innerHTML = questions[0][3];
+testQuestions.innerHTML += questions[1][3];
+testQuestions.innerHTML += questions[2][3];
+testQuestions.innerHTML += questions[3][3];
+testQuestions.innerHTML += questions[4][3];
+testQuestions.innerHTML += questions[5][3];
+testQuestions.innerHTML += questions[6][3];
+testQuestions.innerHTML += questions[7][3];
+testQuestions.innerHTML += questions[8][3];
+testQuestions.innerHTML += questions[9][3];
 testQuestions.innerHTML += "<div class=questionContainer id='submitTestArea'>" +
                            "<p> If you are logged in, your tests will be saved. You can access these on your account page. </p>" +
                            "<input id = 'submitTest' class='testButton' type='button' value='Submit Test' onclick='submit()'/> </div>";
+
+for (var i = 0; i < numOfQuestions; i++) {
+  console.log(questions[i][2]);
+}
 
 function submit() {
   //information from answer text boxes are saved as variables
   var correct = 0;
 
-  var answer1 = document.getElementById("answer1").value;
-  var answer2 = document.getElementById("answer2").value;
-  var answer3 = document.getElementById("answer3").value;
-  var answer4 = document.getElementById("answer4").value;
-  var answer5 = document.getElementById("answer5").value;
-  var answer6 = document.getElementById("answer6").value;
+  //array of strings corresponding to answer boxes on the test page
+  var answerVars = ["answer1", "answer2", "answer3", "answer4", "answer5", "answer6", "answer7", "answer8", "", "answer10"];
 
-  var radios = document.getElementsByName('gate');
-  //for loop that loops through the radio options and determines which options is checked
-  //saves that option as an answer
-  for (var i = 0, length = radios.length; i < length; i++)
-  {
-   if (radios[i].checked)
-   {
-    var answer7 = radios[i].value;
-    break;
-   }
+  //looping through answerVars as well as questions[] and adding user answers to questions[]
+  for (var i = 0; i < numOfQuestions; i++) {
+    if(i != 8) {
+      questions[i][4] = document.getElementById(answerVars[i]).value;
+    }
+
+    //last question has a radio option instead of a text box
+    else if (i == 8) {
+      var radios = document.getElementsByName('gate');
+      //for loop that loops through the radio options and determines which options is checked
+      //saves that option as an answer
+      for (var j = 0, length = radios.length; j < length; j++)
+      {
+       if (radios[j].checked)
+       {
+        questions[i][4] = radios[j].value;
+        break;
+       }
+      }
+    }
   }
 
-  var userAnswer1 = question1[1];
-  var userAnswer2 = question2[1];
-  var userAnswer3 = question3[1];
-  var userAnswer4 = question4[1];
-  var userAnswer5 = question5[1];
-  var userAnswer6 = question6[1];
-  var userAnswer7 = question7[1];
+  //comparing user answers to correct ones generated when the questions is generated
+  //incrementing correct if user answer is the same as generated answer
+  for (var i = 0; i < numOfQuestions; i++) {
+    if (questions[i][4] == questions[i][2]) {correct++}
+  }
 
-  if (answer1 == userAnswer1) {correct++}
-  if (answer2 == userAnswer2) {correct++}
-  if (answer3 == userAnswer3) {correct++}
-  if (answer4 == userAnswer4) {correct++}
-  if (answer5 == userAnswer5) {correct++}
-  if (answer6 == userAnswer6) {correct++}
-  if (answer7 == userAnswer7) {correct++}
-
-  console.log("Correct: ", correct);
-
-  testQuestions.innerHTML = "<div class=questionContainer id='resultsDiv'><span class='questionHeader'> Your score: " + correct + "/7 </span>" +
+  testQuestions.innerHTML = "<div class=questionContainer id='resultsDiv'><span class='questionHeader'> Your score: " + correct + "/" + numOfQuestions + " </span>" +
                             "</div>";
 
 }
@@ -79,16 +96,14 @@ function QType1D(questionNumber) {
   var binary2 = (number2).toString(2);
   var paddedBinary = addPadding(binary);
   var paddedBinary2 = addPadding(binary2);
-  // console.log("binary1: ", binary);
-  // console.log("padded binary1: ", paddedBinary);
-  // console.log("binary2: ", binary2);
-  // console.log("padded binary2: ", paddedBinary2);
 
   outputString = "<div class=questionContainer>   <span class='questionHeader' id='questionNumber'>" + questionNumber + " </span> <span class='questionHeader'> Subtract the bottom number from the top number </span>" +
                  "<br><span class='binarySpan'>" + paddedBinary + "</span><br><span class='binarySpan'>" + paddedBinary2 + "</span><br> <br> <label class='testAnswerLabel'> Answer (in decimal) </label> <br> <input type='text' name'answer' id='answer" +
                  questionNumber + "'" + "</div>";
 
-  return [outputString, answer];
+  var question = "Subtract " + paddedBinary2 + " from " + paddedBinary + " (answer in decimal)";
+  var a = questionNumber;
+  return [a, question, answer, outputString];
 }
 
 //Outputs a binary - binary question (answer in binary)
@@ -109,19 +124,14 @@ function QType1B(questionNumber) {
   var paddedBinary = addPadding(binary);
   var paddedBinary2 = addPadding(binary2);
   var answer = addPadding(binaryA);
-  // console.log("binary1: ", binary);
-  // console.log("padded binary1: ", paddedBinary);
-  // console.log("binary2: ", binary2);
-  // console.log("padded binary2: ", paddedBinary2);
-  // console.log("answer decimal: ", a);
-  // console.log("Answer: ", answer);
-
 
   outputString = "<div class=questionContainer>   <span class='questionHeader' id='questionNumber'>" + questionNumber + " </span> <span class='questionHeader'> Subtract the bottom number from the top number </span>" +
                  "<br><span class='binarySpan'>" + paddedBinary + "</span><br><span class='binarySpan'>" + paddedBinary2 + "</span><br> <br> <label class='testAnswerLabel'> Answer (in 8-bit binary) </label> <br> <input type='text' name'answer' id='answer" +
                  questionNumber + "'" + "</div>";
+  var question = "Subtract " + paddedBinary2 + " from " + paddedBinary + " (answer in 8-bit binary)";
+  var a = questionNumber;
 
-  return [outputString, answer];
+  return [questionNumber, question, answer, outputString];
 }
 
 //Outputs a binary + binary question (answer in decimal)
@@ -143,16 +153,14 @@ function QType2D(questionNumber) {
   var binary2 = (number2).toString(2);
   var paddedBinary = addPadding(binary);
   var paddedBinary2 = addPadding(binary2);
-  // console.log("binary1: ", binary);
-  // console.log("padded binary1: ", paddedBinary);
-  // console.log("binary2: ", binary2);
-  // console.log("padded binary2: ", paddedBinary2);
 
   outputString = "<div class=questionContainer>   <span class='questionHeader' id='questionNumber'>" + questionNumber + " </span> <span class='questionHeader'> Add the bottom number to the top number </span>" +
                  "<br><span class='binarySpan'>" + paddedBinary + "</span><br><span class='binarySpan'>" + paddedBinary2 + "</span><br> <br> <label class='testAnswerLabel'> Answer (in decimal) </label> <br> <input type='text' name'answer' id='answer" +
                  questionNumber + "'" + "</div>";
+  var question = "Add " + paddedBinary + " to " + paddedBinary2 + " (answer in decimal)";
+  var a = questionNumber;
 
-  return [outputString, answer];
+  return [a, question, answer, outputString];
 }
 
 //Outputs a binary + binary question (answer in binary)
@@ -176,16 +184,14 @@ function QType2B(questionNumber) {
   var paddedBinary = addPadding(binary);
   var paddedBinary2 = addPadding(binary2);
   var answer = addPadding(binaryA);
-  // console.log("binary1: ", binary);
-  // console.log("padded binary1: ", paddedBinary);
-  // console.log("binary2: ", binary2);
-  // console.log("padded binary2: ", paddedBinary2);
 
   outputString = "<div class=questionContainer>  <span class='questionHeader' id='questionNumber'>" + questionNumber + " </span> <span class='questionHeader'>  Add the bottom number to the top number </span>" +
                  "<br><span class='binarySpan'>" + paddedBinary + "</span><br><span class='binarySpan'>" + paddedBinary2 + "</span><br> <br> <label class='testAnswerLabel'> Answer (in 8-bit binary) </label> <br> <input type='text' name'answer' id='answer" +
                  questionNumber + "'" + "</div>";
+  var question = "Add " + paddedBinary + " to " + paddedBinary2 + " (answer in 8-bit binary)";
+  var a = questionNumber;
 
-  return [outputString, answer];
+  return [a, question, answer, outputString];
 }
 
 //Outputs a binary to decimal conversion question
@@ -200,8 +206,10 @@ function QType3(questionNumber) {
   outputString = "<div class=questionContainer>   <span class='questionHeader' id='questionNumber'>" + questionNumber + " </span> <span class='questionHeader'>  Convert the following binary into decimal </span>" +
                  "<br><span class='binarySpan'>" + paddedBinary + "</span><br> <br> <label class='testAnswerLabel'> Answer (in decimal) </label> <br> <input type='text' name'answer' id='answer" +
                  questionNumber + "'" + "</div>";
+  question = "Convert " + paddedBinary + " to decimal";
+  var a = questionNumber;
 
-  return [outputString, answer];
+  return [a, question, answer, outputString];
 }
 
 //Outputs a decimal to binary conversion question
@@ -216,8 +224,10 @@ function QType4(questionNumber) {
   outputString = "<div class=questionContainer>   <span class='questionHeader' id='questionNumber'>" + questionNumber + " </span> <span class='questionHeader'> Convert the following decimal into binary </span>" +
                  "<br><span class='binarySpan'>" + number + "</span><br> <br> <label class='testAnswerLabel'> Answer (in 8-bit binary) </label> <br> <input type='text' name'answer' id='answer" +
                  questionNumber + "'" + "</div>";
+  question = "Convert " + paddedBinary + " to binary (8-bit)";
+  var a = questionNumber;
 
-  return [outputString, answer];
+  return [a, question, answer, outputString];
 }
 
 //Outputs a logic gate image question where the user has to name a logic gate
@@ -237,7 +247,7 @@ function QType5(questionNumber) {
                         "<input type='radio' name='gate' value='not' id='gateInput' /> NOT" +
                         "<input type='radio' name='gate' value='buffer' id='gateInput' /> BUFFER" +
                       "</form>";
-  console.log(number);
+
   if (number == 1) { gateLocation = "<img src= \"/static/imgs/and.png\" >"; var answer = "and" }
   else if (number == 2) { gateLocation = "<img src= \"/static/imgs/nand.png\" >"; var answer = "nand" }
   else if (number == 3) { gateLocation = "<img src= \"/static/imgs/or.png\" >"; var answer = "or" }
@@ -247,14 +257,103 @@ function QType5(questionNumber) {
   else if (number == 7) { gateLocation = "<img src= \"/static/imgs/not.png\" >"; var answer = "not" }
   else if (number == 8) { gateLocation = "<img src= \"/static/imgs/buffer.png\" >"; var answer = "buffer" }
 
-  console.log(gateLocation);
-  console.log(answer);
-
   outputString = "<div class=questionContainer>   <span class='questionHeader' id='questionNumber'>" + questionNumber + " </span> <span class='questionHeader'> Name the following logic gate </span>" +
                  "<br><div class='gateDiv'>" + gateLocation + " <br><label class='testAnswerLabel'> Answer </label> <br>" + radioOptions +
                  "</div>";
+  question = "Name the following logic gate: " + gateLocation;
+  var a = questionNumber;
+  return [a, question, answer, outputString];
+}
 
-  return [outputString, answer];
+function QType6(questionNumber) {
+  var outputString = "";
+  var inputs = 0;
+  var gateLocation = "";
+  var answer = "";
+  var number = randomNumber(1,8);
+  var input1 = randomNumber(0,1);
+  var input2 = randomNumber(0,1);
+
+  if (number == 1) {
+    gateLocation = "<img src= \"/static/imgs/and.png\" >";
+    if (input1 == 1 && input2 == 1 ) {answer = 1}
+    else {answer = 0}
+    inputs = 2;
+  }
+
+  else if (number == 2) {
+    gateLocation = "<img src= \"/static/imgs/nand.png\" >";
+    if (input1 == 1 && input2 == 1 ) {answer = 0}
+    else {answer = 1}
+    inputs = 2;
+  }
+
+  else if (number == 3) {
+    gateLocation = "<img src= \"/static/imgs/or.png\" >";
+    if (input1 == 0 && input2 == 0) {answer = 0}
+    else {answer = 1}
+    inputs = 2;
+  }
+
+  else if (number == 4) {
+    gateLocation = "<img src= \"/static/imgs/nor.png\" >";
+    if (input1 == 1 && input2 == 1) {answer = 0}
+    else {answer = 1}
+    inputs = 2;
+  }
+
+  else if (number == 5) {
+    gateLocation = "<img src= \"/static/imgs/xor.png\" >";
+    if (input1 == 0 && input2 == 0) {answer = 0}
+    else if (input1 == 1 && input2 == 1) {answer = 0}
+    else if (input1 == 1 && input2 == 0) {answer = 1}
+    else if (input1 == 0 && input2 == 1) {answer = 1}
+    inputs = 2;
+  }
+
+  else if (number == 6) {
+    gateLocation = "<img src= \"/static/imgs/xnor.png\" >";
+    if (input1 == 0 && input2 == 0) {answer = 1}
+    else if (input1 == 1 && input2 == 1) {answer = 1}
+    else if (input1 == 1 && input2 == 0) {answer = 0}
+    else if (input1 == 0 && input2 == 1) {answer = 0}
+    inputs = 2;
+  }
+
+  else if (number == 7) {
+    gateLocation = "<img src= \"/static/imgs/not.png\" >";
+    if (input1 == 1) {answer = 0}
+    else if (input1 == 0) {answer = 1}
+    inputs = 1;
+  }
+
+  else if (number == 8) {
+    gateLocation = "<img src= \"/static/imgs/buffer.png\" >";
+    if (input1 == 1) {answer = 1}
+    else if (input1 == 0) {answer = 0}
+    inputs = 1;
+  }
+
+  if (inputs == 2) {
+    outputString = "<div class=questionContainer>   <span class='questionHeader' id='questionNumber'>" + questionNumber +
+                   " </span> <span class='questionHeader'> Give the output of the following gate, given input 1 = " + input1 +
+                   " and input 2 = " + input2 + " </span>" +
+                   "<br><div class='gateDiv'>" + gateLocation + " <br><label class='testAnswerLabel'> Answer </label> <br>" +
+                   "</div>";
+    question = "Give the output of the following gate, given input 1 = " + input1 + " and input 2 = " + input2 + gateLocation;
+  }
+  else if (inputs == 1) {
+    outputString = "<div class=questionContainer>   <span class='questionHeader' id='questionNumber'>" + questionNumber +
+                   " </span> <span class='questionHeader'> Give the output of the following gate, given input 1 = " + input1 + " </span>" +
+                   "<br><div class='gateDiv'>" + gateLocation + " <br><label class='testAnswerLabel'> Answer </label> <br>" +
+                   "</div>";
+    question = "Give the output of the following gate, given input 1 = " + input1 + gateLocation;
+  }
+
+  question = "Name the following logic gate: " + gateLocation;
+  var a = questionNumber;
+  console.log(question, questionNumber, answer, outputString);
+  return [a, question, answer, outputString];
 }
 
 //Adds padding to a binary number if number < 8 bits in length

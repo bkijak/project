f529ba82ab1f631b109a7f66c52f6336efc61cf5
Bartlomiej Kijak <b.kijak27@gmail.com>
2018-03-30 14:53:26 +0100

    added a textual representation to the binary demonstration

--- static/scripts/binary.js
+++ static/scripts/binary.js
@@ -43,57 +43,84 @@ $(document).ready(function(){
 
 //goes through columns of binary arrays and determines the carry and answer for
 //each one of them, calls displayBinary() within each loop. The loop has a delay
-//determined by the user
+// determined by the user
 function runDemo(binary1, binary2, carry, answer, i, delay) {
     var finished = 0;
+    var text = "";
     if (i < 0) {
       finished = 1;
       displayBinary(binary1, binary2, carry, answer, i, finished);
+      document.getElementById('algoInfo').innerHTML = "Demonstration finished";
       return;
     }
 
     setTimeout(function () {
       if(binary1[i] == 0 && binary2[i] == 0) {
         if(carry[i] == 0 || carry[i] == 'empty') {
-          carry[i-1] = 0;
+          carry[i-1] = 'empty';
           answer[i] = 0;
+          text = "<p><span class='bin'> 0 </span> + <span class='bin'> 0 </span> " +
+           "<p></p> Carry = <span class='carry'> none </span> <p></p> Answer = <span class='answer'> 0 </span> <p></p>" +
+           "Next carry = <span class='carry'> none </span></p>"
         }
         if(carry[i] == 1) {
           answer[i] = 1;
+          text = "<p><span class='bin'> 0 </span> + <span class='bin'> 0 </span> " +
+           "<p></p> Carry = <span class='carry'> 1 </span> <p></p> Answer = <span class='answer'> 1 </span> <p></p>" +
+           "Next carry = <span class='carry'> none </span></p>"
         }
       }
       if(binary1[i] == 1 && binary2[i] == 0){
         if(carry[i] == 0 || carry[i] == 'empty') {
-          carry[i-1] = 0;
+          carry[i-1] = 'empty';
           answer[i] = 1;
+          text = "<p><span class='bin'> 1 </span> + <span class='bin'> 0 </span> " +
+           "<p></p> Carry = <span class='carry'> none </span> <p></p> Answer = <span class='answer'> 1 </span> <p></p>" +
+           "Next carry = <span class='carry'> none </span></p>"
         }
         if(carry[i] == 1) {
           answer[i] = 0;
           carry[i-1] = 1;
+          text = "<p><span class='bin'> 1 </span> + <span class='bin'> 0 </span> " +
+           "<p></p> Carry = <span class='carry'> 1 </span> <p></p> Answer = <span class='answer'> 0 </span> <p></p>" +
+           "Next carry = <span class='carry'> 1 </span></p>"
         }
       }
       if(binary1[i] == 0 && binary2[i] == 1) {
         if(carry[i] == 0 || carry[i] == 'empty') {
-          carry[i-1] = 0;
+          carry[i-1] = 'empty';
           answer[i] = 1;
+          text = "<p><span class='bin'> 0 </span> + <span class='bin'> 1 </span> " +
+           "<p></p> Carry = <span class='carry'> none </span> <p></p> Answer = <span class='answer'> 1 </span> <p></p>" +
+           "Next carry = <span class='carry'> 0 </span></p>"
         }
         if(carry[i] == 1) {
           answer[i] = 0;
           carry[i-1] = 1;
+          text = "<p><span class='bin'> 0 </span> + <span class='bin'> 1 </span> " +
+           "<p></p> Carry = <span class='carry'> none </span> <p></p> Answer = <span class='answer'> 0 </span> <p></p>" +
+           "Next carry = <span class='carry'> 1 </span></p>"
         }
       }
       if(binary1[i] == 1 && binary2[i] == 1){
         if(carry[i] == 0 || carry[i] == 'empty') {
           carry[i-1] = 1;
           answer[i] = 0;
+          text = "<p><span class='bin'> 1 </span> + <span class='bin'> 1 </span> " +
+           "<p></p> Carry = <span class='carry'> none </span> <p></p> Answer = <span class='answer'> 0 </span> <p></p>" +
+           "Next carry = <span class='carry'> 1 </span></p>"
         }
         if(carry[i] == 1) {
           answer[i] = 1;
           carry[i-1] = 1;
+          text = "<p><span class='bin'> 1 </span> + <span class='bin'> 1 </span> " +
+           "<p></p> Carry = <span class='carry'> 1 </span> <p></p> Answer = <span class='answer'> 1 </span> <p></p>" +
+           "Next carry = <span class='carry'> 1 </span></p>"
         }
       }
 
-      console.log('Generated answer: ' + answer)
+
+      document.getElementById('algoInfo').innerHTML = text;
       displayBinary(binary1, binary2, carry, answer, i, finished);
 
       runDemo(binary1, binary2, carry, answer, --i, delay);
@@ -114,60 +141,67 @@ function displayBinary(binary1, binary2, carry, answer, column, finished) {
     // for loop looping through all the columns within the binary table
     for(var i = 0; i < 8; i++) {
       if(i != column) {
-        row1 += td1 + binary1[i] + ctd;
-        row2 += td1 + binary2[i] + ctd;
+        row1[i] = td1 + binary1[i] + ctd;
+        row2[i] = td1 + binary2[i] + ctd;
         if(carry[i] == 'empty') {
-          row3 += td1 + ctd;
+          row3[i] = td1 + ctd;
         }
         else {
-          row3 += td1 + carry[i] + ctd;
+          row3[i] = td1 + carry[i] + ctd;
         }
         if(answer[i] == 'empty') {
-          row4 += td1 + ctd;
+          row4[i] = td1 + ctd;
         }
         else {
-          row4 += td1 + answer[i] + ctd;
+          row4[i] = td1 + answer[i] + ctd;
         }
       }
       else if(i == column) {
         //if the loop in the function calling this function has not finished
         if(!finished) {
-          row1 += td2 + binary1[i] + ctd;
-          row2 += td2 + binary2[i] + ctd;
+          row1[i] = td2 + binary1[i] + ctd;
+          row2[i] = td2 + binary2[i] + ctd;
           if(carry[i] == 'empty') {
-            row3 += td2 + ctd;
+            row3[i] = td2 + ctd;
           }
           else {
-            row3 += td2 + carry[i] + ctd;
+            row3[i] = td2 + carry[i] + ctd;
           }
           if(answer[i] == 'empty') {
-            row4 += td2 + ctd;
+            row4[i] = td2 + ctd;
           }
           else {
-            row4 += td2 + answer[i] + ctd;
+            row4[i] = td2 + answer[i] + ctd;
           }
         }
         //if the loop in the function calling this function has finished
         else {
-          row1 += td1 + binary1[i] + ctd;
-          row2 += td1 + binary2[i] + ctd;
+          row1[i] = td1 + binary1[i] + ctd;
+          row2[i] = td1 + binary2[i] + ctd;
           if(carry[i] == 'empty') {
-            row3 += td1 + ctd;
+            row3[i] = td1 + ctd;
           }
           else {
-            row3 += td1 + carry[i] + ctd;
+            row3[i] = td1 + carry[i] + ctd;
           }
           if(answer[i] == 'empty') {
-            row4 += td1 + ctd;
+            row4[i] = td1 + ctd;
           }
           else {
-            row4 += td1 + answer[i] + ctd;
+            row4[i] = td1 + answer[i] + ctd;
           }
         }
 
       }
     }
     //writing generated rows which are arrays of columns to HTML
+    row1=row1.join("");
+    row2=row2.join("");
+    row3=row3.join("");
+    row4=row4.join("");
+    // console.log(row1);
+    console.log(row3);
+
     document.getElementById('binary1').innerHTML = row1;
     document.getElementById('binary2').innerHTML = row2;
     document.getElementById('carry').innerHTML = row3;
